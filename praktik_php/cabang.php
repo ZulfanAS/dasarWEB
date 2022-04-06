01817e841c53c0b09beec271738f2fe7a5c8803b
ZulfanAkmal <zulfanakmalsantoso@gmail.com>
2022-04-06 13:20:37 +0700

    dasarWeb

---
+++ praktik_php/cabang.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $nama = "budi";
+
+            if($nama=="andi"){
+                echo "selamat belajar $nama";
+            }
+        ?>
+    </body>
+</html>
---
+++ praktik_php/ulang3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            for ($i=1; $i<10; $i++){
+                echo "anda urutan ke-$i <br>";
+            }
+            ?>
+    </body>
+</html>
