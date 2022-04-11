78530d8784e5c986d7430c11affa666332d95db4
ZulfanAkmal <zulfanakmalsantoso@gmail.com>
2022-04-11 09:54:40 +0700

    dasarWeb

---
+++ FormFile/postForm.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        Selamat Datang <?php echo $_POST["myname"]; ?> !!<br>
+        Dari <?php echo $_POST["myaddress"];?>
+    </body>
+</html>
