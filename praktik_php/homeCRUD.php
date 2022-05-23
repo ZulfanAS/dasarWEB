1416445c088a41f84cdd3de8f442f78712b96af8
ZulfanAkmal <zulfanakmalsantoso@gmail.com>
2022-05-23 15:02:14 +0700

    dasarWeb

--- praktik_php/homeCRUD.php
+++ praktik_php/homeCRUD.php
@@ -1,44 +1,45 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>HOME</title>
-        <link rel="stylesheet" type="text/css" href="setail.css"/>
-    </head>
-    <body>
-        <h1>DATA MAHASISWA</h1>
-        <a href="insertForm.html"><button>TAMBAH DATA</button></a>
+<head><title>Home</title>
+<link rel="stylesheet" type="text/css" href="style.css"/></head>
+<body>
+    <h1>DATA MAHASISWA</h1>
+    <a style="text-decoration: none"; href="insertForm.html">
+    <input type="submit" value="Tambah Data" /></a>
         <table>
             <tr>
-                <th>ID</th>
-                <th>NAMA</th>
-                <th>ALAMAT</th>
-                <th>Aksi</th>
+                <th> ID </th>
+                <th> Nama </th>
+                <th> Alamat </th>
+                <th> Foto </th>
+                <th> Aksi </th>
             </tr>
             <?php
-                include "myconnection.php";
-                $query = "SELECT * FROM student";
-                $result = mysqli_query($connect, $query);
-
-                if(mysqli_num_rows($result) > 0){
-                    while($row = mysqli_fetch_array($result)){
-                        ?>
-                        <tr>
-                            <td><?php echo $row["id"]; ?></td>
-                            <td><?php echo $row["name"];?></td>
-                            <td><?php echo $row["address"];?></td>
-                            <td>
-                                <a href="editForm.php?id=<?php echo $row["id"];?> ">
-                                <button>edit</button></a>
-                                <a href="delete.php?id=<?php echo $row["id"];?> ">
-                                <button>hapus</button></a>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                }else {
-                    echo "0 results";
+            include "myconnection.php";
+            $query = "SELECT * FROM student";
+            $result = mysqli_query($connect,$query);
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+            ?>
+            <tr>
+                <td> <?php echo $row["id"]; ?> </td>
+                <td> <?php echo $row["name"]; ?> </td>
+                <td> <?php echo $row["address"]; ?> </td>
+                <td> <img src="Uploads/<?php echo $row["image"]; ?>" width="100" alt="image"> </td>
+                <td>
+                    <a href="editForm.php?id=<?php echo $row["id"];?>">
+                    <button>Edit</button></a> 
+                    <a href="delete.php?id=<?php echo $row["id"];?>">
+                    <button>Hapus</button></a>
+                </td>
+            </tr>
+            <?php
                 }
-                ?>
+            }
+            else{
+                echo "0 result";
+            }
+            ?>
         </table>
-    </body>
+</body>
 </html>
--- praktik_php/insertProcess.php
+++ praktik_php/insertProcess.php
@@ -1,17 +1,25 @@
 <?php
     include "myconnection.php";
+    $target_path="Uploads/";
 
-    $name = $_GET["myname"];
-    $address = $_GET["myaddress"];
+    $target_path=$target_path.basename($_FILES['myfoto']['name']);
 
-    $query = "INSERT INTO student(name, address)
-            VALUE('$name', '$address')";
-
-    if(mysqli_query($connect, $query)){
-        echo "data baru berhasil ditambahkan";
-    } else {
-        echo "data baru gagal ditambahkan <br>". mysqli_error($connect);
+    if (move_uploaded_file($_FILES['myfoto']['tmp_name'],$target_path)) {
+        echo "The file ". basename($_FILES['myfoto']['name'])." has been uploaded <br><br>";
+    }else {
+        echo "There was an error uploading the file, please try again!";
     }
+    $name = $_POST["myname"];
+    $address = $_POST["myaddress"];
+    $image = $_FILES["myfoto"]['name'];
 
-    mysqli_close($connect);
+    $query = "INSERT INTO student (name,address,image) VALUES
+    ('$name', '$address', '$image')";
+
+if(mysqli_query($connect, $query)){
+    echo "Data baru berhasil ditambahkan";
+}else{
+    echo "Data baru gagal ditambahkan <br>" . mysqli_error($connect);
+}
+mysqli_close($connect);
 ?>
