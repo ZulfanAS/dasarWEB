d667c7b49b2a3a401ab53f3957a81f3890a1f379
ZulfanAkmal <zulfanakmalsantoso@gmail.com>
2022-05-23 14:03:28 +0700

    dasarWeb

--- Cookies/prosesBeli.php
+++ Cookies/prosesBeli.php
@@ -2,6 +2,6 @@
     if (isset ($_POST["gula"]) && isset($_POST["minyak"])){
         setcookie("jumlahgula", $_POST["gula"]);
         setcookie("jumlahminyak",$_POST["minyak"]);
-        header("location ; keranjang.php");
+        header("location:keranjang.php");
     }
 ?>
