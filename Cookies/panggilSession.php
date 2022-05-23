07ee2236e3659290b2289fee49fc0e171f46f289
ZulfanAkmal <zulfanakmalsantoso@gmail.com>
2022-05-23 14:48:20 +0700

    dasarWeb

--- Cookies/panggilSession.php
+++ Cookies/panggilSession.php
@@ -4,10 +4,10 @@
 <html>
     <body>
       <?php 
-      echo "Makanan favorit saya adalah" . $_SESSION["makananfav"]."<br>";
-      echo "Minuman favorit saya adalah" . $_SESSION["minumanfav"]. "<br>";
+      echo "Makanan favorit saya adalah " . $_SESSION["makananfav"]."<br>";
+      echo "Minuman favorit saya adalah " . $_SESSION["minumanfav"]. "<br>";
 
-      print_r($_SESSION);
+      //print_r($_SESSION);
     ?>  
     </body>
 </html>
